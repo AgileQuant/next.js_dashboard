52bb4239b37c4301881efa016d76888881ab3da4
oq <unlimitedpassion@163.com>
2024-05-09 20:49:09 +0800

    添加身份验证

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,31 @@ import { sql } from '@vercel/postgres'; // 这里需要注意
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
+// ...
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    console.info('Authenticating... formData', formData);
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
+
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
